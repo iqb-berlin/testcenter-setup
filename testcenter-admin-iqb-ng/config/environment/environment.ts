a6f8967e711638b98e772238a37c07bace0de1e1
paflov <paf@titelfrei.de>
2019-08-26 17:32:29 +0200

    added testcenter-admin frontend - docker file / submodule / config - dockerfile works, sompose configuration is not ready

---
+++ testcenter-admin-iqb-ng/config/environment/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    testcenterUrl: 'http://localhost:9090/admin/',
+    appName: 'IQB-Testcenter Verwaltung',
+    appPublisher: 'IQB - Institut zur Qualitätsentwicklung im Bildungswesen',
+    appVersion: 'docker version'
+};
