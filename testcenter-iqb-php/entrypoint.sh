7f9529e052fb0fc4566b7133466554e36f0eb5e3
paflov <paf@titelfrei.de>
2019-08-28 16:25:30 +0200

    includer newer & better initlize script for backend

--- testcenter-iqb-php/entrypoint.sh
+++ testcenter-iqb-php/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 rm /var/www/html/vo_code/DBConnectionData.json
 echo "{\"type\": \"mysql\", \"host\": \"${MYSQL_HOST}\", \"port\": \"${MYSQL_PORT}\", \"dbname\": \"${MYSQL_DATABASE}\", \"user\": \"${MYSQL_USER}\", \"password\": \"${MYSQL_PASSWORD}\"}" >> /var/www/html/vo_code/DBConnectionData.json
-php /var/www/html/create/createSuperuser.cli.php --user_name=$SUPERUSER_NAME --user_password=$SUPERUSER_PASSWORD
+php /var/www/html/scripts/initialize.php --user_name=$SUPERUSER_NAME --user_password=$SUPERUSER_PASSWORD --workspace=$WORKSPACE_NAME --test_login_name=$TEST_LOGIN_NAME --test_login_password=$TEST_LOGIN_PASSWORD
+chown -R www-data:www-data /var/www/html/vo_data
 apache2-foreground
