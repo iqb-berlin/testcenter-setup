e150c4552ab772d6c524ce6a82c9866b3ce3fc70
paf <philipp.franck@dainst.de>
2020-05-11 16:01:22 +0200

    rewrite present envitonemnt.ts instead of holding a separate one

--- testcenter-frontend/config/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  testcenterUrl: 'http://localhost:9090/',
-  appName: 'IQB-Testcenter',
-  appPublisher: 'IQB - Institut zur Qualitätsentwicklung im Bildungswesen',
-  appVersion: 'docker-version'
-};
