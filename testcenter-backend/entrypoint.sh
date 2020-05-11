5035d292f55284c132b4a41fa55b7f55888234e4
paflov <philipp.franck@dainst.de>
2020-05-11 12:15:35 +0200

    update backend to 3.0.2

--- testcenter-backend/entrypoint.sh
+++ testcenter-backend/entrypoint.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
+
+# db connection
 rm /var/www/html/vo_code/DBConnectionData.json
-echo "{\"type\": \"mysql\", \"host\": \"${MYSQL_HOST}\", \"port\": \"${MYSQL_PORT}\", \"dbname\": \"${MYSQL_DATABASE}\", \"user\": \"${MYSQL_USER}\", \"password\": \"${MYSQL_PASSWORD}\"}" >> /var/www/html/vo_code/DBConnectionData.json
+echo "{\"type\": \"mysql\", \"host\": \"${MYSQL_HOST}\", \"port\": \"${MYSQL_PORT}\", \"dbname\": \"${MYSQL_DATABASE}\", \"user\": \"${MYSQL_USER}\", \"password\": \"${MYSQL_PASSWORD}\"}" > /var/www/html/config/DBConnectionData.json
+
+# init data
 php /var/www/html/scripts/initialize.php --user_name=$SUPERUSER_NAME --user_password=$SUPERUSER_PASSWORD --workspace=$WORKSPACE_NAME --test_login_name=$TEST_LOGIN_NAME --test_login_password=$TEST_LOGIN_PASSWORD
+
+# composer dependencies
+sh scripts/install_composer.sh
+php composer.phar install
+
+# file-rights
 chown -R www-data:www-data /var/www/html/vo_data
+
+# keep container open
 apache2-foreground
