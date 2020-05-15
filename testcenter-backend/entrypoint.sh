18b9bbd606efb39497c2dcdd49e38d228f19f4b4
paflov <paf@titelfrei.de>
2020-05-15 18:36:34 +0200

    use init-script for creating db, install composer dependencies and more

--- testcenter-backend/entrypoint.sh
+++ testcenter-backend/entrypoint.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
 # db connection
-rm /var/www/html/vo_code/DBConnectionData.json
-echo "{\"type\": \"mysql\", \"host\": \"${MYSQL_HOST}\", \"port\": \"${MYSQL_PORT}\", \"dbname\": \"${MYSQL_DATABASE}\", \"user\": \"${MYSQL_USER}\", \"password\": \"${MYSQL_PASSWORD}\"}" > /var/www/html/config/DBConnectionData.json
+DBConnectionDataFile=/var/www/html/config/DBConnectionData.json
+if [ ! -f "$DBConnectionDataFile" ]; then
+  touch $DBConnectionDataFile
+  echo "{\"type\": \"mysql\", \"host\": \"${MYSQL_HOST}\", \"port\": \"${MYSQL_PORT}\", \"dbname\": \"${MYSQL_DATABASE}\", \"user\": \"${MYSQL_USER}\", \"password\": \"${MYSQL_PASSWORD}\"}" > $DBConnectionDataFile
+fi
+
 
 # init data
-php /var/www/html/scripts/initialize.php --user_name=$SUPERUSER_NAME --user_password=$SUPERUSER_PASSWORD --workspace=$WORKSPACE_NAME --test_login_name=$TEST_LOGIN_NAME --test_login_password=$TEST_LOGIN_PASSWORD
-
-# composer dependencies
-sh scripts/install_composer.sh
-php composer.phar install
+php /var/www/html/scripts/initialize.php \
+  --user_name=$SUPERUSER_NAME \
+  --user_password=$SUPERUSER_PASSWORD \
+  --workspace=$WORKSPACE_NAME \
+  --test_login_name=$TEST_LOGIN_NAME \
+  --test_login_password=$TEST_LOGIN_PASSWORD \
+  --test_person_codes="xxx yyy"
 
 # file-rights
 chown -R www-data:www-data /var/www/html/vo_data
